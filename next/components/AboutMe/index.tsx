203aa3b3a789254db44f53281bad97d17d5c0da9
ALexis Brun <alexisbn@outlook.com>
2025-03-06 15:09:34 +0100

    fix svg

--- next/components/AboutMe/index.tsx
+++ next/components/AboutMe/index.tsx
@@ -4,14 +4,18 @@ import { useRef, useEffect } from 'react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import gsap from 'gsap';
 
-const phrase = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio eum dignissimos similique alias, obcaecati animi temporibus eveniet nam tempora rerum, maiores cum maxime aliquam! Odio cumque reprehenderit fuga harum? Fugit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae eum error, odio corporis hic cumque nulla facilis ullam veniam delectus sunt ipsum alias sit obcaecati labore fuga sed corrupti ipsam.";
+const phrase =
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio eum dignissimos similique alias, obcaecati animi temporibus eveniet nam tempora rerum, maiores cum maxime aliquam! Odio cumque reprehenderit fuga harum? Fugit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae eum error, odio corporis hic cumque nulla facilis ullam veniam delectus sunt ipsum alias sit obcaecati labore fuga sed corrupti ipsam.";
 
 export default function AboutMe() {
-  const refs = useRef<HTMLSpanElement[]>([]);
+  const wordsRef = useRef<HTMLParagraphElement[]>([]);
   const aboutContainerRef = useRef<HTMLDivElement>(null);
   const titleRef = useRef<HTMLHeadingElement>(null);
   const textContentRef = useRef<HTMLDivElement>(null);
   const textContainerRef = useRef<HTMLDivElement>(null);
+
+  // Réinitialise le tableau pour éviter les doublons
+  wordsRef.current = [];
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -19,7 +23,8 @@ export default function AboutMe() {
   }, []);
 
   const createAnimation = () => {
-    gsap.to(refs.current, {
+    // Animation des mots (au lieu des lettres)
+    gsap.to(wordsRef.current, {
       scrollTrigger: {
         trigger: titleRef.current,
         scrub: true,
@@ -41,21 +46,18 @@ export default function AboutMe() {
       '--border-scale': 1,
       ease: 'power2.out',
     });
-
   };
 
   const splitWords = (phrase: string) =>
     phrase.split(' ').map((word, i) => (
-      <p key={`${word}_${i}`}>{splitLetters(word)}</p>
-    ));
-
-  const splitLetters = (word: string) =>
-    word.split('').map((letter, i) => (
-      <span key={`${letter}_${i}`} ref={(el) => {
-        if (el) refs.current.push(el);
-      }}>
-        {letter}
-      </span>
+      <p
+        key={`${word}_${i}`}
+        ref={(el) => {
+          if (el) wordsRef.current.push(el);
+        }}
+      >
+        {word}
+      </p>
     ));
 
   return (
