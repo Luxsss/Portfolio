0da402753b6c7ef33e4e1b9e5eae807d5769f674
ALexis Brun <alexisbn@outlook.com>
2025-03-08 20:32:15 +0100

    fse

--- next/components/Footer/index.tsx
+++ next/components/Footer/index.tsx
@@ -4,9 +4,9 @@ import Content from '@/components/Footer/content'
 
 export default function Navbar() {
   return (
-    <div className='relative h-[200px]' style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}} >
-      <div className='relative h-[calc(100vh+200px)] -top-[100vh]'>
-        <div className='h-[200px] sticky top-[calc(100vh-200px)]'>
+    <div className='relative h-[250px]' style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}} >
+      <div className='relative h-[calc(100vh+250px)] -top-[100vh]'>
+        <div className='h-[250px] sticky top-[calc(100vh-250px)]'>
           <Content />
         </div>
       </div>
